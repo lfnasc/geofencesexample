0422a00e7460f8bef78aceab6ea828adcce9d807
Leonardo Nascimento <leonardo.stenofh@gmail.com>
2017-05-09 15:49:52 -0400

    Added GeofencingRequest builder method

--- app/src/main/java/com/apps/stenofh/geofencesexample/MainActivity.java
+++ app/src/main/java/com/apps/stenofh/geofencesexample/MainActivity.java
@@ -7,7 +7,12 @@ import android.os.Bundle;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingRequest;
 import com.google.android.gms.location.LocationServices;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener{
 
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
      * */
 
     private GoogleApiClient mGoogleApiClient;
+    private List<Geofence> mGeofenceList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +37,52 @@ public class MainActivity extends AppCompatActivity implements GoogleApiClient.C
 
         // setting google api services
         setGoogleApiClient();
+
+        // setting geofences of placeholder points
+        setGeofences();
+
+        // specifying geofences triggers
+        GeofencingRequest geofencingRequest = getGeofencingRequest();
+
+    }
+
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder builder = new GeofencingRequest.Builder();
+        builder.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER);
+        builder.addGeofences(mGeofenceList);
+        return builder.build();
+    }
+
+    private void setGeofences() {
+        double[] lat = new double[5];
+        double[] lng = new double[5];
+        mGeofenceList = new ArrayList<>();
+
+        lat[0] = -3.095049;
+        lng[0] = -59.995185;
+        lat[1] = -3.094489;
+        lng[1] = -59.995712;
+        lat[2] = -3.096849;
+        lng[2] = -59.992190;
+        lat[3] = -3.092792;
+        lng[3] = -59.997341;
+        lat[4] = -3.093772;
+        lng[4] = -59.993252;
+
+        for (int i = 0; i < 5; i++) {
+            mGeofenceList.add(new Geofence.Builder()
+                    .setRequestId("Teste [" + i + "]")
+                    .setCircularRegion(
+                            lat[i],
+                            lng[i],
+                            10
+                    )
+                    .setExpirationDuration(30000)
+                    .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER |
+                            Geofence.GEOFENCE_TRANSITION_EXIT)
+                    .build()
+            );
+        }
     }
 
     private void setGoogleApiClient() {
