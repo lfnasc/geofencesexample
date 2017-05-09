40d7538565bce86a7bbe74a977744a0461ed4558
Leonardo Nascimento <leonardo.stenofh@gmail.com>
2017-05-09 14:14:12 -0400

    Added placeholders points for test

--- app/src/main/java/com/apps/stenofh/geofencesexample/MainActivity.java
+++ app/src/main/java/com/apps/stenofh/geofencesexample/MainActivity.java
@@ -5,6 +5,17 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /***
+     *
+     * placeholders points
+     * -3.095049, -59.995185
+     * -3.094489, -59.995712
+     * -3.096849, -59.992190
+     * -3.092792, -59.997341
+     * -3.093772, -59.993252
+     *
+     * */
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
