c2fcf90f488111fda5e5ee100dc4e48bb5d7e434
Leonardo Nascimento <leonardo.stenofh@gmail.com>
2017-05-10 14:38:58 -0400

    Fixed bug GeofenceActivity opened with a blank screen

--- app/src/main/java/com/apps/stenofh/geofencesexample/GeofenceActivity.java
+++ app/src/main/java/com/apps/stenofh/geofencesexample/GeofenceActivity.java
@@ -1,7 +1,6 @@
 package com.apps.stenofh.geofencesexample;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
@@ -12,8 +11,8 @@ import android.support.v7.app.AppCompatActivity;
 public class GeofenceActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onCreate(savedInstanceState, persistentState);
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_geofence);
     }
 }
